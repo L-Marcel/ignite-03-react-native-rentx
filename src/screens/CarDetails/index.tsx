ff6eb2f94a66aef790a552c9b1aaff76faca078b
l-marcel <lmgh1312@gmail.com>
2023-01-16 18:51:12 -0300

    style: changing car details and images

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -123,13 +123,7 @@ export function CarDetails({}: CarDetailsProps) {
           })}
         </Accessories>
 
-        <About>
-          {car.about}
-          {car.about}
-          {car.about}
-          {car.about}
-          {car.about}
-        </About>
+        <About>{car.about}</About>
       </Animated.ScrollView>
 
       <Footer>
